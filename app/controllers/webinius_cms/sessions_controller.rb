e15cc86a51eef36b50208f0ad0bc26fe06ca57df
hadi-air <pophadi@gmail.com>
2014-11-09 16:59:55 +0100

    updates login page

--- app/controllers/webinius_cms/sessions_controller.rb
+++ app/controllers/webinius_cms/sessions_controller.rb
@@ -11,16 +11,16 @@ module WebiniusCms
 		  user = User.where(username: params[:username]).first
 		  if user && user.authenticate(params[:password])
 		    session[:user_id] = user.id
-		    redirect_to pages_url, notice: "Sie sind jetzt eingeloggt!"
+		    redirect_to pages_url, notice: "You are now logged in!"
 		  else
-		    flash[:error] = "Falsches Email oder Passwort"
+		    flash[:error] = "Wrong email or password."
 		    render "new"
 		  end
 		end
 
 		def destroy
 		  session[:user_id] = nil
-		  redirect_to root_url, notice: "Sie sind jetzt ausgeloggt!"
+		  redirect_to root_url, notice: "Logout successfull!"
 		end
 
 	end
--- lib/webinius_cms/engine.rb
+++ lib/webinius_cms/engine.rb
@@ -3,7 +3,7 @@ module WebiniusCms
     isolate_namespace WebiniusCms
 
     config.active_record.schema_format = :sql
-    config.assets.precompile += %w( webinius_cms/admin.css webinius_cms/admin.js webinius_cms/sites.css webinius_cms/sites.js tinymce/tinymce.min.js )
+    config.assets.precompile += %w( webinius_cms/admin.css webinius_cms/admin.js webinius_cms/sites.css webinius_cms/sites.js tinymce/tinymce.min.js webinius_cms/login.css )
     I18n.config.enforce_available_locales = false
   end
 end
