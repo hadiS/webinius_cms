19b99ea5647996f4e001f80943a44731dd50f8d7
hadi-air <pophadi@gmail.com>
2014-11-10 11:28:45 +0100

    minor fixes

--- app/helpers/webinius_cms/application_helper.rb
+++ app/helpers/webinius_cms/application_helper.rb
@@ -6,6 +6,17 @@ module WebiniusCms
 				content_tag :div, content_tag(:span, model.errors[attribute].first, class: 'control-label'), class: 'field_with_errors'
 			end
 		end
+
+		def list_nested_pages(pages)
+			pages.map do |page, sub_pages|
+				render(page) + list_nested_pages(sub_pages)
+			end.join.html_safe
+		end
+
+		def set_lang(receiver, method_name)
+			text = receiver.send("#{I18n.locale}_#{method_name}")
+			text.present? ? text : receiver.send("de_#{method_name}")
+		end
 	
 	end
 end
--- app/helpers/webinius_cms/pages_helper.rb
+++ app/helpers/webinius_cms/pages_helper.rb
@@ -1,16 +1,5 @@
 module WebiniusCms
 	module PagesHelper
-		
-		def list_nested_pages(pages)
-			pages.map do |page, sub_pages|
-				render(page) + list_nested_pages(sub_pages)
-			end.join.html_safe
-		end
 
-		def set_lang(receiver, method_name)
-			text = receiver.send("#{I18n.locale}_#{method_name}")
-			text.present? ? text : receiver.send("de_#{method_name}")
-		end
-		
 	end
 end
