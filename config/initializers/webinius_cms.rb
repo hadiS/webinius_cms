28d7fe885f49eeda15785026c881f614fb9e2eec
hadi-air <pophadi@gmail.com>
2014-11-17 22:58:31 +0100

    fixes minor bugs

---
+++ config/initializers/webinius_cms.rb
@@ -0,0 +1,1 @@
+# WebiniusCms::ApplicationController.send(:include, Rails.application.routes.url_helpers)
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   mount WebiniusCms::Engine => "/"
+
+  post 'contact_mail', to: 'mailings#send_contact_email', as: 'contact_mail'
 end
