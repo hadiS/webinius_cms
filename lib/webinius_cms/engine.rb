f45881e95ce6f96bd065349f2df6fe181d97b869
hadi-air <pophadi@gmail.com>
2014-11-18 15:42:26 +0100

    adds engine as isolated namespace

--- lib/webinius_cms/engine.rb
+++ lib/webinius_cms/engine.rb
@@ -1,6 +1,6 @@
 module WebiniusCms
   class Engine < ::Rails::Engine
-    # isolate_namespace WebiniusCms
+    isolate_namespace WebiniusCms
 
     config.active_record.schema_format = :sql
     config.assets.precompile += %w( webinius_cms/admin.css webinius_cms/admin.js webinius_cms/sites.css webinius_cms/sites.js tinymce/tinymce.min.js webinius_cms/login.css )
