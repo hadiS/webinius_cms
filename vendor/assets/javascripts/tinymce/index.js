b1d86d830bedbdac013bec7ee3210a081f268c40
hadi-air <pophadi@gmail.com>
2015-02-20 18:03:54 +0100

    Fix for tinymce

--- vendor/assets/javascripts/tinymce/index.js
+++ vendor/assets/javascripts/tinymce/index.js
@@ -2,5 +2,5 @@
 //= require tinymce/plugins/link/plugin.min
 //= require tinymce/plugins/textcolor/plugin.min
 //= require tinymce/plugins/table/plugin.min
-//= require tinymce/plugins/textcolor/plugin.min
+//= require tinymce/plugins/code/plugin.min
 //= require tinymce/themes/modern/theme.min
