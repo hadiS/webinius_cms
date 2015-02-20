0693a20208e047919d6e7471bbca23a611fa2dd6
hadi-air <pophadi@gmail.com>
2015-02-20 17:57:07 +0100

    fix for tinmyce

--- vendor/assets/javascripts/tinymce/index.js
+++ vendor/assets/javascripts/tinymce/index.js
@@ -1,4 +1,6 @@
 //= require tinymce/tinymce.min
 //= require tinymce/plugins/link/plugin.min
 //= require tinymce/plugins/textcolor/plugin.min
+//= require tinymce/plugins/table/plugin.min
+//= require tinymce/plugins/textcolor/plugin.min
 //= require tinymce/themes/modern/theme.min
